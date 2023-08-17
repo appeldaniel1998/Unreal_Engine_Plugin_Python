0262a81dffe76b6dea1c498815a5a6d272d2b901
Daniel Appel <appel.daniel1998@gmail.com>
2023-08-18 00:15:56 +0300

    added keyboard drone nav

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import socket
 import time
 
+from publicDroneControl import PublicDroneControl
+import keyboard
+
 if __name__ == '__main__':
-    ip_portRecv = ("127.0.0.1", 3001)
-    udp_socketRecv = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # receiver socket (UDP) opened
-    udp_socketRecv.bind(ip_portRecv)
+    publicDroneControl = PublicDroneControl("127.0.0.1", 3001)
 
-    udp_socketSend = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # sender socket (UDP) opened
-    ip_portSend = ("127.0.0.1", 3002)
-
-    print("Waiting for message...")
     while True:
-        msg = '{"primitiveControls": {"upAmount": 1, "pitchForwardAmount": 1, "rollRightAmount": 0.5, "yawRightAmount": -0.5}, "getDroneState": "true"}'
-
-        udp_socketSend.sendto(msg.encode('utf-8'), ip_portSend)  # Send message to UE
-
-        message = udp_socketRecv.recv(4096)  # Receive message from UE
-        message_str = message.decode('utf-8')  # Convert message to string
-        print(message_str)
-
-        time.sleep(0.1)
+        if keyboard.is_pressed('w'):
+            publicDroneControl.moveDroneForward(1)
+        if keyboard.is_pressed('s'):
+            publicDroneControl.moveDroneBackward(1)
+        if keyboard.is_pressed('a'):
+            publicDroneControl.moveDroneLeft(1)
+        if keyboard.is_pressed('d'):
+            publicDroneControl.moveDroneRight(1)
+        if keyboard.is_pressed('q'):
+            publicDroneControl.rotateDroneLeft(1)
+        if keyboard.is_pressed('e'):
+            publicDroneControl.rotateDroneRight(1)
+        if keyboard.is_pressed('left shift'):
+            publicDroneControl.moveDroneUp(1)
+        if keyboard.is_pressed('left ctrl'):
+            publicDroneControl.moveDroneDown(1)
+        if keyboard.is_pressed('esc'):
+            break
+        time.sleep(0.01)  # small delay to prevent hogging the CPU
---
+++ publicDroneControl.py
@@ -0,0 +1,46 @@
+import socket
+
+
+class PublicDroneControl:
+    def __init__(self, ip, port):
+        ip_portRecv = (ip, port)
+        self.udp_socketRecv = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # receiver socket (UDP) opened
+        self.udp_socketRecv.bind(ip_portRecv)
+
+        self.udp_socketSend = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # sender socket (UDP) opened
+        self.ip_portSend = (ip, port + 1)
+
+    def send(self, msg):
+        self.udp_socketSend.sendto(msg.encode('utf-8'), self.ip_portSend)  # Send message to UE
+
+    def moveDroneUp(self, multiplier):
+        msg = '{"primitiveControls": {"upAmount": ' + str(multiplier) + '}}'
+        self.send(msg)
+
+    def moveDroneDown(self, multiplier):
+        msg = '{"primitiveControls": {"upAmount": ' + str(-multiplier) + '}}'
+        self.send(msg)
+
+    def moveDroneForward(self, multiplier):
+        msg = '{"primitiveControls": {"pitchForwardAmount": ' + str(multiplier) + '}}'
+        self.send(msg)
+
+    def moveDroneBackward(self, multiplier):
+        msg = '{"primitiveControls": {"pitchForwardAmount": ' + str(-multiplier) + '}}'
+        self.send(msg)
+
+    def moveDroneRight(self, multiplier):
+        msg = '{"primitiveControls": {"rollRightAmount": ' + str(multiplier) + '}}'
+        self.send(msg)
+
+    def moveDroneLeft(self, multiplier):
+        msg = '{"primitiveControls": {"rollRightAmount": ' + str(-multiplier) + '}}'
+        self.send(msg)
+
+    def rotateDroneRight(self, multiplier):
+        msg = '{"primitiveControls": {"yawRightAmount": ' + str(multiplier) + '}}'
+        self.send(msg)
+
+    def rotateDroneLeft(self, multiplier):
+        msg = '{"primitiveControls": {"yawRightAmount": ' + str(-multiplier) + '}}'
+        self.send(msg)
